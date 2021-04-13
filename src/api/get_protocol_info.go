263197126272aab85ebd3ae3dc53d1aa3e231461
zhangshikj <1372362033@qq.com>
2021-04-14 00:49:45 +0800

    Complete the development of packager

--- src/api/get_protocol_info.go
+++ src/api/get_protocol_info.go
@@ -7,22 +7,23 @@ import (
 )
 
 type ProtocolInfo struct {
-	MsfAppId       int
-	SubAppId       int
-	IpVersion      int
-	LocalId        int
-	PingVersion    int
-	SSoVersion     int
-	DbVersion      int
-	MsfSSoVersion  int
-	MiscBitmap     int
-	SubSigMap      int
-	TgtVersion     int
-	PackageName    string
-	PackageVersion string
-	MsfSdkMd5      string
-	BuildTime      int
-	BuildVersion   string
+	MsfAppId        int
+	SubAppId        int
+	IpVersion       int
+	LocalId         int
+	PingVersion     int
+	SSoVersion      int
+	DbVersion       int
+	MsfSSoVersion   int
+	MiscBitmap      int
+	SubSigMap       int
+	TgtVersion      int
+	PackageName     string
+	PackageVersion  string
+	MsfSdkMd5       string
+	BuildTime       int
+	BuildVersion    string
+	ProtocolVersion string
 }
 
 func GetProtocolInfo(isHd bool) *ProtocolInfo {
@@ -38,22 +39,23 @@ func GetProtocolInfo(isHd bool) *ProtocolInfo {
 		version := json.GetString(data, latestKey)
 		info := json.Get(data, version)
 		return &ProtocolInfo{
-			MsfAppId:       json.GetInt(info, "appid"),
-			SubAppId:       json.GetInt(info, "subAppid"),
-			IpVersion:      json.GetInt(info, "ipVersion"),
-			LocalId:        json.GetInt(info, "lId"),
-			PingVersion:    json.GetInt(info, "pingVersion"),
-			SSoVersion:     json.GetInt(info, "ssoVer"),
-			DbVersion:      json.GetInt(info, "dbVer"),
-			MsfSSoVersion:  json.GetInt(info, "msfSsoVer"),
-			MiscBitmap:     json.GetInt(info, "miscBitmap"),
-			SubSigMap:      json.GetInt(info, "subSigMap"),
-			TgtVersion:     json.GetInt(info, "tgtgVer"),
-			PackageName:    json.GetString(info, "packageName"),
-			PackageVersion: json.GetString(info, "packageVersion"),
-			MsfSdkMd5:      json.GetString(info, "sdkMd5"),
-			BuildTime:      json.GetInt(info, "buildTime"),
-			BuildVersion:   json.GetString(info, "buildVersion"),
+			MsfAppId:        json.GetInt(info, "appid"),
+			SubAppId:        json.GetInt(info, "subAppid"),
+			IpVersion:       json.GetInt(info, "ipVersion"),
+			LocalId:         json.GetInt(info, "lId"),
+			PingVersion:     json.GetInt(info, "pingVersion"),
+			SSoVersion:      json.GetInt(info, "ssoVer"),
+			DbVersion:       json.GetInt(info, "dbVer"),
+			MsfSSoVersion:   json.GetInt(info, "msfSsoVer"),
+			MiscBitmap:      json.GetInt(info, "miscBitmap"),
+			SubSigMap:       json.GetInt(info, "subSigMap"),
+			TgtVersion:      json.GetInt(info, "tgtgVer"),
+			PackageName:     json.GetString(info, "packageName"),
+			PackageVersion:  json.GetString(info, "packageVersion"),
+			MsfSdkMd5:       json.GetString(info, "sdkMd5"),
+			BuildTime:       json.GetInt(info, "buildTime"),
+			BuildVersion:    json.GetString(info, "buildVersion"),
+			ProtocolVersion: json.GetString(info, "agreementVersion"),
 		}
 	}
 	log.Default().Println("登录失败，无法获取协议信息")
--- src/util/packet/packet.go
+++ src/util/packet/packet.go
@@ -1,16 +1,19 @@
 package packet
 
 import (
-	"api"
 	"bluesky"
 	"util/bytes"
+	"util/cryptor/tea"
 )
 
-func MakeRequestPacket(cmd string, body, key, tgt []byte, seq int, status byte, info *api.ProtocolInfo) []byte {
+func MakeRequestPacket(cmd string, data, key, tgt, ksid []byte, seq int, status byte, msfAppId int, protocolVersion, androidId *string, needSize bool) []byte {
 	var tgtType = 0
 	if tgt != nil {
 		tgtType = 256
+	} else {
+		tgt = []byte{}
 	}
+	builder := bytes.CreateBuilder()
 	outBuilder := bytes.CreateBuilder()
 	switch status {
 	case bluesky.HasLogin:
@@ -27,11 +30,42 @@ func MakeRequestPacket(cmd string, body, key, tgt []byte, seq int, status byte, 
 		panic("错误状态码，无法进行组包...")
 	}
 	outBuilder.WriteByte(0)
-	if info != nil {
-		println(tgtType)
+	bodyBuilder := bytes.CreateBuilder()
+	headBuilder := bytes.CreateBuilder()
+	if androidId != nil {
+		headBuilder.WriteInt(seq)
+		headBuilder.WriteInt(msfAppId)
+		headBuilder.WriteInt(msfAppId)
+		headBuilder.WriteInt(16777216)
+		headBuilder.WriteInt(0)
+		headBuilder.WriteInt(tgtType)
+		headBuilder.WriteBytesWithSize(tgt, 4)
+		headBuilder.WriteStringWithSize(cmd, 4)
+		headBuilder.WriteBytesWithSize(bytes.RandBytes(4), 4)
+		headBuilder.WriteStringWithSize(*androidId, 4)
+		if ksid == nil {
+			ksid = []byte{}
+		}
+		headBuilder.WriteBytesWithSize(ksid, 4)
+		headBuilder.WriteStringWithShortSize(*protocolVersion, 2)
+		// head.writeInt(4);
+		// 跳过ECDH
 	} else {
-
+		headBuilder.WriteStringWithSize(cmd, 4)
+		headBuilder.WriteBytesWithSize(bytes.RandBytes(4), 4)
+		headBuilder.WriteInt(4)
 	}
-
-	return outBuilder.Bytes()
+	head := headBuilder.Bytes()
+	bodyBuilder.WriteInt(len(head) + 4)
+	bodyBuilder.WriteBytes(head)
+	if needSize {
+		bodyBuilder.WriteInt(len(data) + 4)
+	}
+	bodyBuilder.WriteBytes(data)
+	eData := tea.NewCipher(key).Encrypt(bodyBuilder.Bytes())
+	outBuilder.WriteBytes(eData)
+	body := outBuilder.Bytes()
+	builder.WriteInt(len(body) + 4)
+	builder.WriteBytes(body)
+	return builder.Bytes()
 }
