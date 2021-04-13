4ccf8fce33ab680601eb866343b22d40b06e4dea
zhangshikj <1372362033@qq.com>
2021-04-13 18:58:47 +0800

    完成接包器

--- src/bluesky/receive.go
+++ src/bluesky/receive.go
@@ -18,13 +18,28 @@ type Packet struct {
 	Uin  int
 	Body []byte
 
+	isOver bool
+
 	Element *list.Element
 }
 
-func (r *Receiver) AddWaiter(cmd string, seq int) {
-	pack := Packet{Cmd: cmd, Seq: seq}
-	r.waiter.PushFront(&pack)
+func (r *Receiver) AddWaiter(cmd string, seq int) *list.Element {
+	pack := Packet{Cmd: cmd, Seq: seq, isOver: false}
+	return r.waiter.PushFront(&pack)
+}
 
+func (r *Receiver) WaitPacket(elem *list.Element) *Packet {
+	waiter, ok := elem.Value.(*Packet)
+	if ok {
+		for waiter.isOver == false {
+			if waiter.isOver == true {
+				break
+			}
+		}
+		return waiter
+	} else {
+		panic("这个屑玩意不是一个接包器~")
+	}
 }
 
 // InitReceive 初始化借包器
